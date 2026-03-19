799e308ba1ed561a2a67d28bd6b74573ccf4d7eb
student01 <student01@conoship.com>
2026-03-19 16:25:03 +0100

    XGBoost Model added

---
+++ models/xgb_stability_baseline.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_absolute_error
+
+# Load Data
+df = pd.read_csv("C:/Users/student01/Desktop/data/all_ships_baseline.csv")
+
+# Define Features (X) and Target (y)
+features = ['draft', 'vcg', 'displacement', 'condition_code','trim','openings_per_compartment','total_compartments']
+X = df[features]
+y = df['target_margin']
+
+# Data Split (80% train, 20% test)
+X_Train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and Train the Model
+model = XGBRegressor(n_estimators=100, learning_rate=0.1, max_depth=6, random_state=42)
+model.fit(X_Train, y_train)
+print("Model training complete.")
+
+# Evaluate Performance
+predictions = model.predict(X_test)
+accuracy = r2_score(y_test, predictions)
+error = mean_absolute_error(y_test, predictions)
+
+print(f"Model Performance")
+print(f"R-squared score: {accuracy:.4f}")
+print(f"Average Error (m): {error:.4f}")
+
