da9ac5528e35fbfff552bbe8134aca44ac0d9853
Student02 <student02@conoship.com>
2026-03-19 16:16:35 +0100

    importance of openings per compartment

--- data_visualization/all_ships_feature_importance.py
+++ data_visualization/all_ships_feature_importance.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('C:/Users/student01/Desktop/data/all_ships_baseline_clean.csv')
-loading_cols = ['draft', 'vcg', 'displacement', 'condition_code', 'target_margin']
+df = pd.read_csv('C:/Users/student02/data/allships.csv')
+loading_cols = ['draft', 'vcg', 'displacement', 'condition_code','trim','openings_per_compartment','total_compartments', 'target_margin']
 
 # Importance for loading factors only
 importance = df[loading_cols].corr()['target_margin'].abs().sort_values(ascending=False).drop('target_margin')
@@ -12,6 +12,6 @@ importance = df[loading_cols].corr()['target_margin'].abs().sort_values(ascendin
 plt.figure(figsize=(10, 6))
 sns.barplot(x=importance.values, y=importance.index, palette='viridis')
 plt.title('Importance: How Loading Conditions Drive Stability')
-plt.savefig('C:/Users/student01/Desktop/data/plots/all_ships/all_ships_loading_features_importance.png')
+plt.savefig('C:/Users/student02/data/plots/all_ships_loading_features_importance.png')
 plt.close()
 print("File saved!")
