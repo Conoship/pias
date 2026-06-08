1700e14c53203eb6658f4f0359fbcf1d15800ce2
stephanosiliadis <Stefanos06@outlook.com.gr>
2026-06-08 19:00:47 +0200

    Re-wrote the formula for Partial Subdiv in more intuitive order

--- src/app/controllers/agent_pipeline_controller.py
+++ src/app/controllers/agent_pipeline_controller.py
@@ -260,7 +260,7 @@ class AgentPipelineController(object):
                     "Subdivision Length": subdivision_length,
                     "Light Service Draft": light_service_draft,
                     "Partial Subdivision": (
-                        (-light_service_draft + subdivision_draft) * 0.6
+                        (subdivision_draft - light_service_draft) * 0.6
                     )
                     + light_service_draft,
                     "Subdivision Draft": subdivision_draft,
